9e653af25038ad0366e65086864645f0142bcac6
Toshiki Kamei <kame0008@gmail.com>
2022-09-03 12:08:46 +0200

    fix dev server port

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -3,24 +3,9 @@ import { Layout } from '~/components/layout'
 const Index = () => {
   return (
     <Layout>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/blog" rel="noreferrer">
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/tutorials/jokes" rel="noreferrer">
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
+      <div className={'h-full grid items-center justify-items-center text-3xl font-black'}>
+        <h1>WHITE ALBUM</h1>
+      </div>
     </Layout>
   )
 }
